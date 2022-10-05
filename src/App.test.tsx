092bee8eb06d0b0d7daa10238b69a36de16e291d
SteveReid <steve@appby.design>
2022-10-05 16:52:47 +0100

    configure react router and vite test runner, add a home and not found page, and some unit tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,16 +1,43 @@
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import { describe, expect, it } from 'vitest';
 
-import App from './App';
+import { App, WrappedApp } from './App';
 
 describe('App', () => {
-  it('should render hello world', () => {
-    render(<App />);
+  it('should render title, React Vite Starter', () => {
+    render(<WrappedApp />);
 
     expect(
       screen.getByRole('heading', {
         level: 1,
       })
-    ).toHaveTextContent('Hello World!');
+    ).toHaveTextContent('React Vite Starter');
+  });
+
+  it('should render heading, Home Page', () => {
+    render(<WrappedApp />);
+
+    expect(
+      screen.getByRole('heading', {
+        level: 2,
+      })
+    ).toHaveTextContent('Home Page');
+  });
+});
+
+describe('WrappedApp', () => {
+  it('should render not found if invalid path', () => {
+    render(
+      <MemoryRouter initialEntries={['/wrong-path']}>
+        <App />
+      </MemoryRouter>
+    );
+
+    expect(
+      screen.getByRole('heading', {
+        level: 2,
+      })
+    ).toHaveTextContent('Not Found!?!');
   });
 });
